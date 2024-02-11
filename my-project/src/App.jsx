d3d08b4f73cf35573ff3952832988bcb3c353be7
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-02-11 12:52:20 -0500

    commit

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navbar } from "./components"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className='blog text-6xl underline'>
-        Velpblog
-      </h1>
-    </>
+    <div>
+      LL
+
+    </div>
+
   )
 }
+
 
 export default App
